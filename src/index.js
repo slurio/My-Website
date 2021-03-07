1779ffff3efee1ad283ed1fb19f619eaf579b582
slurio <samantha.lurio@gmail.com>
2021-03-07 13:27:32 -0500

    added scroll to top function after each new render

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './assets/font/Assistant-VariableFont_wght.ttf';
+import ScrollToTop from './Components/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollToTop>
+        <App/>
+      </ScrollToTop>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
