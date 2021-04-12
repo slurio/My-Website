a8bb970eccfb8d2b5d06cdb4d03e3a95a74c788e
slurio <samantha.lurio@gmail.com>
2021-04-12 13:28:49 -0700

    image path

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './assets/font/Assistant-VariableFont_wght.ttf';
-import ScrollToTop from './Components/ScrollToTop';
+import '../../assets/font/Assistant-VariableFont_wght.ttf';
+import ScrollToTop from '../../Components/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
